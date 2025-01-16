ce0727b17f242bf1ae6876719cc2cbc5579ed6f9
Song Jae Won <jwsonghub@gmail.com>
2025-01-16 21:20:40 +0900

    Design: 버튼 디자인

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,6 +11,8 @@ export default {
       colors: {
         background: 'var(--background)',
         foreground: 'var(--foreground)',
+        slateGray: '#566270',
+        neutralGray: '#AFAFAF',
       },
     },
   },
