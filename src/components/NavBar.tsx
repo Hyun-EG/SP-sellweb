1a2b53a2edf622872a91d1afb32bc9f44c696af9
Song Jae Won <jwsonghub@gmail.com>
2025-02-04 16:44:07 +0900

    Design: 드롭다운 애니메이션 추가 및 라우팅 추가

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { useState, useEffect, useRef } from 'react';
+import Link from 'next/link';
 import Image from 'next/image';
 import SubNav from './SubNav';
 import logo from '../../public/svgs/icon-logo.svg';
@@ -15,9 +16,38 @@ const NavBar = () => {
     setIsClick((prev) => (prev === dropdown ? false : dropdown));
   };
 
-  const templateItems = ['템플릿 소개', '기능 소개'];
-  const supportItems = ['1:1문의', '이용안내'];
-  const myPageItems = ['구매내역', '문의내역', '내 정보 수정', '찜 목록'];
+  const templateItems = [
+    { label: '템플릿 소개', href: '/temp' },
+    { label: '기능 소개', href: '/features' },
+  ];
+  const supportItems = [
+    {
+      label: '1:1 문의',
+      href: '/ask',
+    },
+    {
+      label: '문의하기',
+      href: '/contact',
+    },
+  ];
+  const myPageItems = [
+    {
+      label: '구매내역',
+      href: '/mypage/bought',
+    },
+    {
+      label: '문의내역',
+      href: '/mypage/ask',
+    },
+    {
+      label: '내 정보 수정',
+      href: '/mypage/edit',
+    },
+    {
+      label: '찜 목록',
+      href: '/mypage/like',
+    },
+  ];
 
   useEffect(() => {
     const handleClickOutside = (event: MouseEvent) => {
@@ -39,17 +69,21 @@ const NavBar = () => {
   return (
     <>
       <header className="flex justify-between items-center shadow-md h-[80px] relative">
-        <div className="flex items-center justify-center ml-12 ">
-          <Image
-            src={logo}
-            alt="메인 로고"
-            className="w-[137px] h-[36px] cursor-pointer "
-          />
-        </div>
+        <Link href={'/'}>
+          <div className="flex items-center justify-center ml-12 ">
+            <Image
+              src={logo}
+              alt="메인 로고"
+              className="w-[137px] h-[36px] cursor-pointer "
+            />
+          </div>
+        </Link>
         <ol className="flex justify-start items-center space-x-4 w-3/5 ">
-          <li className="flex items-center justify-center w-[120px] h-[80px] text-center cursor-pointer ">
-            셀웹
-          </li>
+          <Link href={'/'}>
+            <li className="flex items-center justify-center w-[120px] h-[80px] text-center cursor-pointer ">
+              셀웹
+            </li>
+          </Link>
           <li
             onClick={() => toggleDropdown('template')}
             className="flex items-center justify-center w-[120px] h-[80px] text-center cursor-pointer "
@@ -63,7 +97,7 @@ const NavBar = () => {
             {isClick === 'template' && (
               <div
                 ref={dropdownRef}
-                className="absolute top-full mt-2 w-[255px] bg-white text-white z-50 rounded-lg shadow-md  "
+                className="absolute top-full mt-2 w-[255px] bg-white text-white z-50 rounded-lg shadow-md animate-rotateMenu origin-top-center"
               >
                 <SubNav items={templateItems} />
               </div>
@@ -82,7 +116,7 @@ const NavBar = () => {
             {isClick === 'support' && (
               <div
                 ref={dropdownRef}
-                className="absolute top-full mt-2 w-[255px] bg-white text-white z-50 rounded-lg shadow-md"
+                className="absolute top-full mt-2 w-[255px] bg-white text-white z-50 rounded-lg shadow-md animate-rotateMenu origin-top-center"
               >
                 <SubNav items={supportItems} />
               </div>
@@ -101,7 +135,7 @@ const NavBar = () => {
             {isClick === 'myPage' && (
               <div
                 ref={dropdownRef}
-                className="absolute top-full mt-2 w-[255px] bg-white text-white z-50 rounded-lg shadow-md"
+                className="absolute top-full mt-2 w-[255px] bg-white text-white z-50 rounded-lg shadow-md animate-rotateMenu origin-top-center"
               >
                 <SubNav items={myPageItems} />
               </div>
@@ -109,12 +143,16 @@ const NavBar = () => {
           </li>
         </ol>
         <ol className="flex justify-center items-center space-x-2 w-1/5 pl-8">
-          <li className="flex items-center justify-center w-[120px] h-[80px] text-center cursor-pointer">
-            로그인
-          </li>
-          <li className="flex items-center justify-center w-[120px] h-[80px] text-center cursor-pointer">
-            회원가입
-          </li>
+          <Link href="/login">
+            <li className="flex items-center justify-center w-[120px] h-[80px] text-center cursor-pointer">
+              로그인
+            </li>
+          </Link>
+          <Link href="/signup">
+            <li className="flex items-center justify-center w-[120px] h-[80px] text-center cursor-pointer">
+              회원가입
+            </li>
+          </Link>
           <div className="flex items-center justify-center w-[80px] h-[80px]">
             <Image
               src={darkMode}
