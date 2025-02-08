6fb8db1c4efee96cdfd85dd57f01c73b7612486d
Hyun-EG <codiee@naver.com>
2025-02-08 21:24:38 +0900

    Fix: Email => userId로 변경

--- models/User.ts
+++ models/User.ts
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 const userSchema = new mongoose.Schema({
   userName: { type: String, require: true },
-  email: { type: String, require: true, unique: true },
+  userId: { type: String, require: true, unique: true },
   password: { type: String, require: true },
   phoneNumber: { type: String, require: true, unique: true },
 });
