3374328e497e02f5759b99b0b3db088ddea43884
seonghyun <codiee@naver.com>
2025-02-11 21:43:46 +0900

    Design: 공지사항 페이지 디자인 마무리

--- src/app/notice/page.tsx
+++ src/app/notice/page.tsx
@@ -3,20 +3,21 @@ import TitleBox from '@/components/TitleBox';
 
 export default function Notice() {
   const rows = [
-    ['홍길동', 'gildong@example.com'],
-    ['김철수', 'chulsoo@example.com'],
-    ['홍길동', 'gildong@example.com'],
-    ['김철수', 'chulsoo@example.com'],
-    ['홍길동', 'gildong@example.com'],
-    ['김철수', 'chulsoo@example.com'],
-    ['홍길동', 'gildong@example.com'],
-    ['김철수', 'chulsoo@example.com'],
-    ['홍길동', 'gildong@example.com'],
-    ['1', 'chulsoo@example.com'],
-    ['2', 'gildong@example.com'],
-    ['3', 'chulsoo@example.com'],
-    ['4', 'gildong@example.com'],
-    ['5', 'chulsoo@example.com'],
+    ['1', '공지사항입니다', '2024.09.01'],
+    ['1', '공지사항입니다', '2024.09.01'],
+    ['1', '공지사항입니다', '2024.09.01'],
+    ['1', '공지사항입니다', '2024.09.01'],
+    ['1', '공지사항입니다', '2024.09.01'],
+    ['1', '공지사항입니다', '2024.09.01'],
+    ['1', '공지사항입니다', '2024.09.01'],
+    ['1', '공지사항입니다', '2024.09.01'],
+    ['1', '공지사항입니다', '2024.09.01'],
+    ['1', '공지사항입니다', '2024.09.01'],
+    ['1', '공지사항입니다', '2024.09.01'],
+    ['1', '공지사항입니다', '2024.09.01'],
+    ['1', '공지사항입니다', '2024.09.01'],
+    ['1', '공지사항입니다', '2024.09.01'],
+    ['1', '공지사항입니다', '2024.09.01'],
   ];
 
   return (
@@ -24,8 +25,9 @@ export default function Notice() {
       <TitleBox title="공지사항" />
       <Table
         headers={[
-          { label: '이름', width: '100px' },
-          { label: '이메일', width: '200px' },
+          { label: '번호', width: '100px' },
+          { label: '제목', width: '900px' },
+          { label: '등록 날짜', width: '200px' },
         ]}
         rows={rows}
       />
