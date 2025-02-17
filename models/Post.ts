5a2f677805079be30002d1b9cf2c88ffcebaa735
seonghyun <codiee@naver.com>
2025-02-17 20:23:51 +0900

    Design: 문의 스키마 모델 작성

---
+++ models/Post.ts
@@ -0,0 +1,11 @@
+import mongoose from 'mongoose';
+
+const PostSchema = new mongoose.Schema({
+  userId: { type: String, required: true },
+  userName: { type: String, required: true },
+  title: { type: String, required: true },
+  content: { type: String, required: true },
+  createdAt: { type: Date, default: Date.now },
+});
+
+export default mongoose.models.Post || mongoose.model('Post', PostSchema);
