610774588947fc984259a5d7c9a7be03d767cacf
seonghyun <codiee@naver.com>
2025-02-20 01:53:27 +0900

    Fix: 경로  바뀐거 반영안돼서 다시 올림

--- src/app/api/get-notice/route.ts
+++ src/app/api/get-notice/route.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from 'next/server';
-import { connectDB } from '../../../../../lib/db';
-import Notice from '../../../../../models/Notice';
+import { connectDB } from '../../../../lib/db';
+import Notice from '../../../../models/Notice';
 
 export async function GET() {
   try {
