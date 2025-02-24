f87437d8df047a60011bee4ff52116f0409a1ca3
seonghyun <codiee@naver.com>
2025-02-24 23:22:36 +0900

    Feat: 문의내역 컴포넌트 추가

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,3 +1,4 @@
+import AdminAsk from '@/components/AdminAsk';
 import AdminNotice from '@/components/AdminNotice';
 import AdminSection from '@/components/AdminSection';
 
@@ -6,14 +7,14 @@ export default function Page() {
     <div className="h-full p-[20px] flex flex-col">
       <div className="w-full h-[400px] flex">
         <AdminSection title="공지사항">
-          <div>
-            <AdminNotice />
-          </div>
+          <AdminNotice />
         </AdminSection>
         <AdminSection title="대시보드">ㄴㅇ</AdminSection>
       </div>
       <div className="w-full h-[400px] flex">
-        <AdminSection title="문의내역">ㄴㅇ</AdminSection>
+        <AdminSection title="문의내역">
+          <AdminAsk />
+        </AdminSection>
         <AdminSection title="템플릿">ㄴㅇ</AdminSection>
       </div>
     </div>
