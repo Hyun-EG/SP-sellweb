1b97332bb10c023997f301011dbfd4ec179f3667
seonghyun <codiee@naver.com>
2025-02-24 23:24:09 +0900

    Feat: 몽고디비 id 불러오게 추가 userName도 추가

--- src/app/api/get-posts/route.ts
+++ src/app/api/get-posts/route.ts
@@ -1,14 +1,15 @@
 import { NextResponse } from 'next/server';
 import { connectDB } from '../../../../lib/db';
 import Post from '../../../../models/Post';
-
 export async function GET() {
   try {
     await connectDB();
 
-    const posts = await Post.find({}).select('title createdAt');
+    const posts = await Post.find({}).select('title createdAt userName _id');
 
     const formattedPosts = posts.map((post) => ({
+      _id: post._id.toString(),
+      userName: post.userName,
       title: post.title,
       createdAt: post.createdAt.toISOString().split('T')[0],
     }));
